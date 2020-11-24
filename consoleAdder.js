866ce27da7484d6b5972aaf21e614b94e5a801d5
vasiliy-klimkin <vasily.klimkin@gmail.com>
2020-11-24 11:31:01 -0500

    refactor

--- consoleAdder.js
+++ consoleAdder.js
@@ -2,33 +2,19 @@
 // which will be numbers, and adds them toghther in and returns a sum.
 // WHEN should i commit?
 
-// command line arguments?
-// how do i remove the first 2 arguments of an array?
-// numbers
-// add numbers toghether 
-// return sum
-
-
-// starting the repo
-// git init
-// git remote add
-// git push
-//////////////////
-// work work work
-// git add
-// git commit
-// git push
-// REPEAT lines 17-20
 console.log("Command Line Arguments!");
+
+const sumArray = function(numbersArr) {
+    let sum = 0;
+    for (let i = 0; i < numbersArr.length; i++) {
+        // i is the index of the number in the array
+        sum = sum + parseInt(numbersArr[i]);
+    }
+    return sum;
+}
+
 const numbers = process.argv.slice(2);
 // numbers is an array
 console.log(numbers);
-let sum = 0;
-for (let i = 0; i < numbers.length; i++) {
-    // i is the index of the number in the array
-    console.log('index is: ', i);
-    console.log('value of that index is: ',parseInt(numbers[i]));
-    sum = sum + parseInt(numbers[i]);
-}
-
-console.log("SUM is: ", sum);
+const answer = sumArray(numbers);
+console.log("result: ", answer);
