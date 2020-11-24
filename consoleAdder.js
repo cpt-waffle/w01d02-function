08d4e583890f42158a17c094ca6ebe1945f853a2
vasiliy-klimkin <vasily.klimkin@gmail.com>
2020-11-24 10:51:15 -0500

    figured out how to use cmd arguments

--- consoleAdder.js
+++ consoleAdder.js
@@ -1,1 +1,12 @@
-console.log('test!');
+// We are tasked to create a program that takes in any number of command line arguments
+// which will be numbers, and adds them toghther in and returns a sum.
+// WHEN should i commit?
+
+// command line arguments?
+// how do i remove the first 2 arguments of an array?
+// numbers
+// add numbers toghether 
+// return sum
+console.log("Command Line Arguments!");
+const numbers = process.argv.slice(2);
+console.log(process.argv.slice(2));
